412d5e6d5353cc02e14b094ff3d5a801e49bca08
makoto hata <makotohata0429@gmail.com>
2024-03-03 23:33:15 +0900

    add: repeat play

--- frontend/app/src/app/components/MusicPlayer.tsx
+++ frontend/app/src/app/components/MusicPlayer.tsx
@@ -4,21 +4,46 @@ import styles from "@/app/components/MusicPlayer.module.scss";
 import Image from "next/image";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 
-type musicInfo = {
+type tracks = {
   title: string;
   artist: string;
   image: string;
-  src: string;
+  source: string;
 };
 
+const userTracksData = [
+  {
+    title: "Summer Walk",
+    artist: "Olexy",
+    image: "music-image.png",
+    source: "summer-walk.mp3",
+  },
+  {
+    title: "Relaxed Vlog (Night Street)",
+    artist: "Ashot-Danielyan-Composer",
+    image: "relaxed-vlog-night-street.webp",
+    source: "relaxed-vlog-night-street.mp3",
+  },
+  {
+    title: "Ambient Piano and Strings",
+    artist: "Daddy_s_Music",
+    image: "ambient-piano-and-strings.png",
+    source: "ambient-piano-and-strings.mp3",
+  },
+];
+
 export default function MusicPlayer() {
-  const imageSrc = "/player/";
-  const [title, setTitle] = useState<string>("Sample Music");
-  const [artist, setArtist] = useState<string>("Sample Artist");
-  const [image, setImage] = useState<string>("music_image.png");
+  const imagePath = "/player/";
+  const trackPath = "/tracks/";
+
+  // TODO バックエンドができたらDBから取得する。
+  const [userTracks, setUserTracks] = useState<tracks[]>(userTracksData);
+
+  const [currentTrack, setCurrentTrack] = useState<tracks>(userTracksData[0]);
+  const [trackIndex, setTrackIndex] = useState<number>(0);
 
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [isAudioPlaying, setIsAudioPlaying] = useState<boolean>(false);
+  const [isAudioPlaying, setIsAudioPlaying] = useState<boolean | null>(null);
 
   const handleTogglePlay = () => {
     if (!isAudioPlaying) {
@@ -32,15 +57,15 @@ export default function MusicPlayer() {
   };
 
   /** 音声の再生時間（s）をstate管理 */
-  const [source, setSource] = useState<string>("summer-walk.mp3");
   const [duration, setDuration] = useState<number>(0);
   const [timePosition, setTimePosition] = useState(0);
+  const [isRepeat, setIsRepeat] = useState<boolean>(false);
 
   /** 音声の再生時間を更新する処理 */
   useEffect(() => {
     setTimePosition(audioRef.current!.currentTime);
     setDuration(audioRef.current!.duration);
-  }, [audioRef.current, setTimePosition]);
+  }, [audioRef, timePosition]);
 
   /** 秒数を「0:00:00」の形式に変換する処理 */
   const getTimeStringFromSeconds = (seconds: number): string => {
@@ -58,8 +83,13 @@ export default function MusicPlayer() {
   };
 
   const handleEnded = () => {
-    setTimePosition(0);
-    setIsAudioPlaying(false);
+    if (isRepeat) {
+      setTimePosition(0);
+      audioRef.current!.play();
+      setIsAudioPlaying(true);
+    } else {
+      handleNext();
+    }
   };
 
   const handleChangeTimePosition = (e: ChangeEvent<HTMLInputElement>) => {
@@ -89,11 +119,47 @@ export default function MusicPlayer() {
   };
 
   const seekBarBackground = {
-    background: `linear-gradient(to right, #C4C4C4 ${100*timePosition/duration}%, #535353 ${100*timePosition/duration}%)`
+    background: `linear-gradient(to right, #C4C4C4 ${
+      (100 * timePosition) / duration
+    }%, #535353 ${(100 * timePosition) / duration}%)`,
   };
 
   const volumeBarBackground = {
-    background: `linear-gradient(to right, #C4C4C4 ${volume}%, #535353 ${volume}%)`
+    background: `linear-gradient(to right, #C4C4C4 ${volume}%, #535353 ${volume}%)`,
+  };
+
+  // スキップの処理
+  const handleNext = () => {
+    if (trackIndex === userTracks.length - 1) {
+      setTrackIndex(0);
+      setCurrentTrack(userTracks[0]);
+    } else {
+      setTrackIndex((prev) => prev + 1);
+      setCurrentTrack(userTracks[trackIndex + 1]);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (trackIndex === 0) {
+      setTrackIndex(userTracks.length - 1);
+      setCurrentTrack(userTracks[-1]);
+    } else {
+      setTrackIndex((prev) => prev - 1);
+      setCurrentTrack(userTracks[trackIndex - 1]);
+    }
+  };
+
+  useEffect(() => {
+    audioRef.current!.src = `${trackPath}${currentTrack.source}`;
+    if (isAudioPlaying !== null) {
+      audioRef.current!.play();
+      setIsAudioPlaying(true);
+    }
+  }, [audioRef, currentTrack]);
+
+  // リピート再生
+  const handleRepeat = () => {
+    setIsRepeat((prev) => !prev);
   };
 
   return (
@@ -101,36 +167,37 @@ export default function MusicPlayer() {
       <div className={styles.musicInfo}>
         <Image
           className={styles.musicImage}
-          src={`${imageSrc}${image}`}
+          src={`${trackPath}${currentTrack.image}`}
           width={70}
           height={70}
           alt="music image"
         ></Image>
         <div className={styles.titleInfo}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.artist}>{artist}</div>
+          <div className={styles.title}>{currentTrack.title}</div>
+          <div className={styles.artist}>{currentTrack.artist}</div>
         </div>
       </div>
       <div className={styles.player}>
         <div className={styles.control}>
           <Image
             className={`${styles.shuffleButton} ${styles.controller}`}
-            src={`${imageSrc}shuffle.png`}
+            src={`${imagePath}shuffle.png`}
             width={24}
             height={24}
             alt="shuffle icon"
           ></Image>
           <Image
             className={`${styles.previousButton} ${styles.controller}`}
-            src={`${imageSrc}previous.png`}
+            src={`${imagePath}previous.png`}
             width={24}
             height={24}
             alt="previous icon"
+            onClick={() => handlePrevious()}
           ></Image>
           <Image
             className={`${styles.playButton} ${styles.controller}`}
             src={
-              isAudioPlaying ? `${imageSrc}pause.png` : `${imageSrc}play.png`
+              isAudioPlaying ? `${imagePath}pause.png` : `${imagePath}play.png`
             }
             width={51}
             height={51}
@@ -141,21 +208,25 @@ export default function MusicPlayer() {
           ></Image>
           <Image
             className={`${styles.nextButton} ${styles.controller}`}
-            src={`${imageSrc}next.png`}
+            src={`${imagePath}next.png`}
             width={24}
             height={24}
             alt="next icon"
+            onClick={() => handleNext()}
           ></Image>
           <Image
             className={`${styles.repeatButton} ${styles.controller}`}
-            src={`${imageSrc}repeat.png`}
+            src={
+              isRepeat ? `${imagePath}one-repeat.png` : `${imagePath}repeat.png`
+            }
             width={24}
             height={24}
             alt="repeat icon"
+            onClick={() => handleRepeat()}
           ></Image>
         </div>
         <audio
-          src={`${imageSrc}${source}`}
+          src={`${trackPath}${currentTrack.source}`}
           ref={audioRef}
           onTimeUpdate={handleTimeUpdate}
           onEnded={handleEnded}
@@ -174,16 +245,14 @@ export default function MusicPlayer() {
             style={seekBarBackground}
           ></input>
           <span className={styles.totalTime}>
-            {getTimeStringFromSeconds(duration)}
+            {duration ? getTimeStringFromSeconds(duration) : "00:00:00"}
           </span>
         </div>
       </div>
       <div className={styles.volume}>
         <Image
           className={`${styles.controlButton} ${styles.controller}`}
-          src={
-            isMute ? `${imageSrc}mute.png` : `${imageSrc}speaker.png`
-          }
+          src={isMute ? `${imagePath}mute.png` : `${imagePath}speaker.png`}
           width={24}
           height={24}
           alt="speaker icon"
