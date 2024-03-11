59653d72b0c86bf4b37e301120a43be7b1503909
makoto hata <makotohata0429@gmail.com>
2024-03-11 23:48:44 +0900

    add: auth middleware

---
+++ frontend/app/src/middleware.ts
@@ -0,0 +1,14 @@
+import { getCurrentUser } from "@/app/utils/UserAPI"
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const currentUser = await getCurrentUser();
+  if (currentUser === null && request.nextUrl.pathname === '/room') {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/room/:path*'],
+}
