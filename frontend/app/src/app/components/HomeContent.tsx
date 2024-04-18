310795ae5d7d592e5c1888ae23d7ef6f4ac9d785
makoto hata <makotohata0429@gmail.com>
2024-04-18 23:39:08 +0900

    add: shop

---
+++ frontend/app/src/app/components/HomeContent.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import styles from "./HomeContent.module.scss";
+import UserInfo from "./UserInfo";
+import RoomSelect from "./RoomSelect";
+import { Music, User, Wallpaper } from "../types/User";
+import Sidebar from "./Sidebar";
+import { Component, useState } from "react";
+import Shop from "@/app/components/shop/Shop";
+import Setting from "./setting/Setting";
+
+export default function HomeContent({
+  currentUser,
+  wallpapers,
+  musics,
+}: {
+  currentUser: User | null;
+  wallpapers: Wallpaper[] | null;
+  musics: Music[] | null;
+}) {
+  const [componentName, setComponentName] = useState<ComponentType>("select");
+
+  // const currentUser = await getCurrentUser();
+  type ComponentType = "select" | "shop" | "setting";
+  const handleComponent = (componentName: ComponentType) => {
+    setComponentName(componentName);
+  };
+
+  if (currentUser !== null && wallpapers !== null && musics !== null) {
+    return (
+      <div className={`${styles.container} background`}>
+        <Sidebar handleComponent={handleComponent} />
+        {currentUser && <UserInfo {...currentUser} />}
+        {componentName === "select" && <RoomSelect />}
+        {componentName === "shop" && <Shop currentUser={currentUser} wallpapers={wallpapers} musics={musics}/>}
+        {componentName === "setting" && <Setting currentUser={currentUser} />}
+      </div>
+    );
+  }
+}
--- frontend/app/src/app/components/RoomSelect.tsx
+++ frontend/app/src/app/components/RoomSelect.tsx
@@ -1,55 +1,50 @@
 import Link from "next/link";
 import styles from "./RoomSelect.module.scss";
 import Image from "next/image";
-import UserInfo from "./UserInfo";
-import { getCurrentUser } from "../utils/UserAPI";
-export default async function RoomSelect() {
-  const currentUser = await getCurrentUser();
+
+export default function RoomSelect() {
 
   return (
-    <div className={`${styles.container} background`}>
-      {currentUser && <UserInfo {...currentUser} />}
-      <div className={styles.selectContainer}>
-        <div className={styles.solo}>
-          <Link href="/soloroom">
-            <Image
-              className={styles.soloImage}
-              src="/solo.png"
-              width={320}
-              height={320}
-              sizes="100vw"
-              style={{
-                maxWidth: 230,
-                height: "auto",
-              }}
-              priority
-              alt="solo"
-            ></Image>
-            <p className={`${styles.roomName} ${styles.roomNameSolo}`}>
-              Solo Room
-            </p>
-          </Link>
-        </div>
-        <div className={styles.multi}>
-          <Link href="/multiroom">
-            {/* <div className={styles.musk}>開 発 中</div> */}
-            <Image
-              className={styles.multiImage}
-              src="/multi.png"
-              width={320}
-              height={320}
-              sizes="100vw"
-              style={{
-                maxWidth: 230,
-                height: "auto",
-              }}
-              priority
-              alt="multi"
-            ></Image>
-            <p className={styles.roomName}>Multi Room</p>
-          </Link>
-        </div>
+    <div className={styles.selectContainer}>
+      <div className={styles.solo}>
+        <Link href="/soloroom">
+          <Image
+            className={styles.soloImage}
+            src="/solo.png"
+            width={320}
+            height={320}
+            sizes="100vw"
+            style={{
+              maxWidth: 230,
+              height: "auto",
+            }}
+            priority
+            alt="solo"
+          ></Image>
+          <p className={`${styles.roomName} ${styles.roomNameSolo}`}>
+            Solo Room
+          </p>
+        </Link>
       </div>
-    </div>
+      <div className={styles.multi}>
+        <Link href="/multiroom">
+          {/* <div className={styles.musk}>開 発 中</div> */}
+          <Image
+            className={styles.multiImage}
+            src="/multi.png"
+            width={320}
+            height={320}
+            sizes="100vw"
+            style={{
+              maxWidth: 230,
+              height: "auto",
+            }}
+            priority
+            alt="multi"
+          ></Image>
+          <p className={styles.roomName}>Multi Room</p>
+        </Link>
+      </div>
+  </div>
   );
 }
