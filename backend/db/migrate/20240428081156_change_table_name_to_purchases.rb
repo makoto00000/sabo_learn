524ee39748147f21fd9429216b253bde1ec412c6
makoto hata <makotohata0429@gmail.com>
2024-04-28 21:14:04 +0900

    fix: migration file

--- backend/db/migrate/20240428081156_change_table_name_to_purchases.rb
+++ backend/db/migrate/20240428081156_change_table_name_to_purchases.rb
@@ -1,6 +1,6 @@
 class ChangeTableNameToPurchases < ActiveRecord::Migration[7.1]
   def change
-    rename_table :music_purchases, :music_purchases
-    rename_table :wallpaper_purchases, :wallpaper_purchases
+    rename_table :music_parchaces, :music_purchases
+    rename_table :wallpaper_parchaces, :wallpaper_purchases
   end
 end
