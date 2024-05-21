4b2ceab094bc131ad9b1973da5e121534719c8f8
makoto hata <makotohata0429@gmail.com>
2024-05-21 13:15:07 +0900

    fix: 変更内容に合わせてrspecを修正

--- backend/spec/requests/api/v1/google_spec.rb
+++ backend/spec/requests/api/v1/google_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'GooglesController' do
+  before do
+    create_list(:music, 3)
+    create(:solo_wallpaper)
+    create(:multi_wallpaper)
+  end
+
   let(:user_count) { User.count }
 
   describe '#callback' do
@@ -27,10 +33,10 @@ RSpec.describe 'GooglesController' do
     end
 
     context 'ログイン失敗' do
-      let(:failed_params) {  {} }
+      let(:failed_params) { {name: nil, email: nil, uid: nil, provider: nil} }
 
       describe '初めてのログイン' do
-        it 'ユーザーが追加されてidをtoken化して返す' do
+        it 'ユーザーが登録されずtokenも返されない' do
           expect { post '/auth/google/callback', params: failed_params }.to change(User, :count).by(0)
           expect(response).to have_http_status(:internal_server_error)
           expect(response.body).not_to include('token')
@@ -38,7 +44,7 @@ RSpec.describe 'GooglesController' do
       end
 
       describe '2回目以降のログイン' do
-        it '2回目以降はメールアドレスが一致するユーザーのidをtoken化して返す' do
+        it 'tokenを返さない' do
           post '/auth/google/callback', params: failed_params
           expect { post '/auth/google/callback', params: failed_params }.to change(User, :count).by(0)
           expect(response).to have_http_status(:internal_server_error)
