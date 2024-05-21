f8f994d853e8f388c2e5b96db4aed46709070388
makoto hata <makotohata0429@gmail.com>
2024-05-21 13:14:40 +0900

    remove: 不要なfactoryを削除

--- backend/spec/factories/music_purchases.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :music_purchase do
-    user { nil }
-    music { nil }
-  end
-end
--- backend/spec/factories/wallpaper_purchases.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :wallpaper_purchase do
-    user { nil }
-    wallpaper { nil }
-  end
-end
